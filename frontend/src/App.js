f39bce6b38350c310f9282919158c85e46e41e3a
Zihnee <mohamed.zihnee070@gmail.com>
2022-11-02 22:43:59 +0500

    01-create-react-app

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">ResellersMV</a>
+      </header>
+      <main>
+        List products
+      </main>
+    </div>
+  );
+}
+
+export default App;
