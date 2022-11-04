1331fadbe2abb7479f0d492e04c0251f483b16cd
Zihnee <mohamed.zihnee070@gmail.com>
2022-11-04 20:19:22 +0500

    12-create-react-context-for-add-item-to-cart

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,8 +4,18 @@ import ProductScreen from "./screens/ProductScreen";
 import Navbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/Container";
 import { LinkContainer } from "react-router-bootstrap";
+import { Link } from "react-router-dom";
+import Badge from "react-bootstrap/Badge";
+import Nav from "react-bootstrap/Nav";
+import { useContext } from "react";
+import { Store } from "./Store";
 
 function App() {
+
+  const {state} = useContext(Store);
+
+  const {cart} = state;
+
   return (
     <BrowserRouter>
       <div className="d-flex flex-column site-container">
@@ -16,6 +26,17 @@ function App() {
               <LinkContainer to="/">
                 <Navbar.Brand>ResellersMV</Navbar.Brand>
               </LinkContainer>
+              <Nav className="me-auto">
+                <Link to="/cart" className="nav-link">
+                  <i className="fa fa-shopping-cart" aria-hidden="true">
+                    {cart.cartItems.length > 0 && (
+                      <Badge pill bg="danger">
+                        {cart.cartItems.length}
+                      </Badge>
+                    )}
+                  </i>
+                </Link>
+              </Nav>
             </Container>
           </Navbar>
         </header>
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { useEffect, useReducer } from "react";
+import { useContext, useEffect, useReducer } from "react";
 import Card from "react-bootstrap/Card";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
@@ -12,6 +12,7 @@ import { Helmet } from "react-helmet-async";
 import LoadingBox from "../components/LoadingBox";
 import MessageBox from "../components/MessageBox";
 import { getError } from "../utils";
+import { Store } from "../Store";
 
 const reducer = (state, action) => {
     switch (action.type) {
@@ -48,6 +49,12 @@ function ProductScreen () {
         };
         fetchData();
     }, [slug]);
+
+    const {state, dispatch: ctxDispatch} = useContext(Store);
+
+    const addToCartHandler = () => {
+        ctxDispatch({type: 'CART_ADD_ITEM', payload: {...product, quantity: 1}});
+    }
 
     return (
         loading ? (
@@ -106,7 +113,7 @@ function ProductScreen () {
                                     {product.countInStock > 0 && (
                                         <ListGroup.Item>
                                             <div className="d-grid">
-                                                <Button variant="primary">
+                                                <Button onClick={addToCartHandler} variant="primary">
                                                     Add to Cart
                                                 </Button>
                                             </div>
